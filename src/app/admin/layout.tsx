5cd1d059a402ae3df606be4c433c4b94e101c9bd
Mike Schutte <tmikeschutte@gmail.com>
2024-11-19 22:56:39 -0500

    default var

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -12,9 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider>
       <SignedOut>
         <SignInButton />
       </SignedOut>
