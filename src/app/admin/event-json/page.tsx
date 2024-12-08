aa7b6fe7394877b88e0e10ee68ea865f757f3024
Mike Schutte <tmikeschutte@gmail.com>
2024-12-08 11:34:44 -0500

    auth checkA

--- src/app/admin/event-json/page.tsx
+++ src/app/admin/event-json/page.tsx
@@ -1,5 +1,10 @@
 import CopyEventJson from "@/components/copy-event-json";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function EventJsonPage() {
+  const { userId } = await auth();
+  if (!userId) return redirect("/admin");
+
   return <CopyEventJson />;
 }
